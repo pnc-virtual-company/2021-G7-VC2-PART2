77bc9968a6e67db83d96f67d39fee9588f12ba87
SOM_CHHORN <som.chhorn@student.passerellesnumeriques.org>
2021-12-19 10:19:35 +0700

    #5 Crud student for back end

---
+++ back/app/Http/Controllers/StudentController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Student::latest()->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'class' => 'required',
+            'phone' => 'min:9 || max:10',
+            'gender' => 'required',
+            'picture' => 'nullable|image|mimes:jpg,jpeg,png,gif,jfif|max:1999'
+        ]);
+        $request->file('picture')->store('public/images');
+        $student = new Student();
+        $student->first_name = $request->first_name;
+        $student->last_name = $request->last_name;
+        $student->class = $request->class;
+        $student->phone = $request->phone;
+        $student->gender = $request->gender;
+        $student->picture = $request->file('picture')->hashName();
+        $student->save();
+
+        return response()->json(['message' => 'Sutdent created successfully'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Student::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'class' => 'required',
+            'phone' => 'required' | 'min:9 || max:10',
+            'gender' => 'required',
+            'picture' => 'nullable|image|mimes:jpg,jpeg,png,gif,jfif|max:2000'
+        ]);
+        $request->file('picture')->store('public/images');
+        $student = Student::findOrFail($id);
+        $student->first_name = $request->first_name;
+        $student->last_name = $request->last_name;
+        $student->class = $request->class;
+        $student->phone = $request->phone;
+        $student->gender = $request->gender;
+        $student->picture = $request->file('image')->hashName();
+        $student->save();
+
+        return response()->json(['message' => 'Sutdent updated successfully'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $isDeleted = Student::destroy($id);
+        if ($isDeleted == 1) {
+            return response()->json(['massage' => 'Deleted'], 200);
+        } else {
+            return response()->json(['massage' => 'Not Found'], 404);
+        }
+    }
+    
+    public function search($first_name)
+    {
+        return Student::where('first_name', 'like', '%' . $first_name . '%')->get();
+    }
+
+}
---
+++ back/app/Models/Student.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'class',
+        'phone',
+        'picture',
+        'gender'
+    ];
+    // protected $hidden = [
+    //     'created_at',
+    //     'updated_at',
+    // ];
+}
