9e3a80cca55992786c029ab13a1735a81e35c6db
SOM_CHHORN <som.chhorn@student.passerellesnumeriques.org>
2021-12-19 10:51:38 +0700

    initialize project

--- back/routes/api.php
+++ back/routes/api.php
@@ -4,9 +4,3 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 
-Route::get('students', [StudentController::class, 'index']);
-Route::get('students/{id}', [StudentController::class, 'show']);
-Route::post('students', [StudentController::class, 'store']);
-Route::put('students/{id}', [StudentController::class, 'update']);
-Route::delete('students/{id}', [StudentController::class, 'destroy']);
-Route::get('/students/search/{first_name}', [StudentController::class, 'search']);
