6373f569ec24e969358d1a4383a7d68d4cd60ea0
lanhphalla <lanh.phalla@student.passerellesnumeriques.org>
2021-12-21 16:00:27 +0700

    #14 signin

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -4,7 +4,22 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-  
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/components/form/FormLogin.vue')
+  },
+  // {
+  //   path: '/user',
+  //   name: 'User',
+  //   component: () => import('@/components/views/User.vue')
+  // },
+  {
+    path: '/navbar',
+    name: 'Navbar',
+    component: () => import('@/components/nav/Navbar.vue')
+  },
+
 ]
 
 const router = new VueRouter({
