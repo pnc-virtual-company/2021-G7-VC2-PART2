6a62d8affc0f9f2874a2af572c018b8290e88d1c
lanhphalla <lanh.phalla@student.passerellesnumeriques.org>
2021-12-30 18:14:57 +0700

    #00

--- front/src/components/nav/Navbar.vue
+++ front/src/components/nav/Navbar.vue
@@ -1,18 +1,73 @@
 <template>
-  <nav >
+  <nav>
+    <v-navigation-drawer v-model="sidebar" app absolute left temporary>
+      <v-list>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <div class="navRes">
+            <v-btn class="mb-3" text
+              active-class="grey-5 white--text"
+              
+              v-if="appTitle.role === 'admin'"
+            >
+              <v-icon left class="blue--text">mdi-home</v-icon>
+              <router-link to="/user" tag="span" style="cursor: pointer">
+                User
+              </router-link>
+            </v-btn>
+            <v-btn active-class="grey-5 white--text" class="mb-3" text>
+              <v-icon left class="blue--text">mdi-account-multiple</v-icon>
+              <router-link to="/studentlist" tag="span" style="cursor: pointer">
+                Students
+              </router-link>
+            </v-btn>
+            <v-btn active-class="grey-5 white--text" class="mb-3" text>
+              <v-icon left class="blue--text">mdi-clipboard-check</v-icon>
+              <router-link to="/permission" tag="span" style="cursor: pointer">
+                Permissions
+              </router-link>
+            </v-btn>
+            <v-btn active-class="grey-5 white--text" class="mb-3" text>
+              <v-icon left class="blue--text">mdi-file-document</v-icon>
+              <router-link to="/disciples" tag="span" style="cursor: pointer">
+                Disciples
+              </router-link>
+            </v-btn>
+            <v-btn active-class="grey-5 white--text" @click="logOut" text>
+              <v-icon left class="blue--text">mdi-login</v-icon>
+              <router-link to="/logout" tag="span" style="cursor: pointer">
+                Log Out
+              </router-link>
+            </v-btn>
+          </div>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
     <v-toolbar class="orange lighten-1">
       <span class="hidden-sm-and-up">
         <v-app-bar-nav-icon @click="sidebar = !sidebar"> </v-app-bar-nav-icon>
       </span>
       <v-toolbar-title class="d-flex">
-          <v-list-item-avatar color="grey darken-3">
-              <v-img class="elevation-6" alt="" src="https://zenprospect-production.s3.amazonaws.com/uploads/pictures/60b386ff84a3630001b3bb0d/picture"></v-img>
-          </v-list-item-avatar>
-          <v-toolbar-title class="text-uppercase mt-4"> {{appTitle.userName}}</v-toolbar-title>
+        <v-list-item-avatar color="grey darken-3">
+          <v-img
+            class="elevation-6"
+            alt=""
+            src="https://zenprospect-production.s3.amazonaws.com/uploads/pictures/60b386ff84a3630001b3bb0d/picture"
+          ></v-img>
+        </v-list-item-avatar>
+        <v-toolbar-title class="text-uppercase mt-4">
+          {{ appTitle.userName }}</v-toolbar-title
+        >
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn active-class="grey-5 white--text" text v-if="appTitle.role === 'admin'">
+        <v-btn
+          active-class="grey-5 white--text"
+          text
+          v-if="appTitle.role === 'admin'"
+        >
           <v-icon left class="blue--text">mdi-home</v-icon>
           <router-link to="/user" tag="span" style="cursor: pointer">
             User
@@ -48,33 +103,33 @@
 </template>
 
 <script>
-
 export default {
   data() {
     return {
-      appTitle: JSON.parse(localStorage.getItem("user")),
+      appTitle: JSON.parse(localStorage.getItem("userLogin")),
       sidebar: false,
       group: false,
     };
   },
   methods: {
     logOut() {
-      
-      localStorage.clear("userId");
+      localStorage.getItem("userLogin");
+      localStorage.clear("userLogin");
       this.$emit("log_out");
-      this.$router.push("/login");
-     
+      this.$router.push("/login").catch(() => {});
     },
   },
-  
-  
-
 };
 </script>
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Pushster&family=Raleway:wght@300&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Pushster&family=Raleway:wght@300&display=swap");
 nav {
-font-family: 'Pushster', cursive};
-
-
+  font-family: "Pushster", cursive;
+}
+.navRes {
+  display: grid;
+  justify-content: start;
+  margin: 3px;
+  
+}
 </style>
