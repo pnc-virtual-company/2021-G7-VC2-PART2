adc5bbdc97f29437f00cf4bde4c82504d3f68234
SOM_CHHORN <som.chhorn@student.passerellesnumeriques.org>
2022-01-02 18:39:31 +0700

    #33 Update route for discipline

--- back/app/Models/Discipline.php
+++ back/app/Models/Discipline.php
@@ -9,7 +9,7 @@ class Discipline extends Model
 {
     use HasFactory;
     protected $fillable = ['discipline_type', 'date_time', 'explaination'];
-    public function Permission(){
+    public function students(){
         return $this->belongsTo(Student::class, 'student_id');
     }
 
