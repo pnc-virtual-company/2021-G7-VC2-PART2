066e995ee82ac4d149298f90a76921c2fc77c37e
SOM_CHHORN <som.chhorn@student.passerellesnumeriques.org>
2022-01-03 22:40:42 +0700

    #29 Search discipline

--- front/src/components/ui/disciplinedata/DisciplineCard.vue
+++ front/src/components/ui/disciplinedata/DisciplineCard.vue
@@ -3,13 +3,13 @@
     <template>
       <v-simple-table> </v-simple-table>
       <div class="permission">
-        <v-card 
-        color="grey lighten-4" 
-        class="mx-auto card"
-         max-width="900"
-         v-for="(discipline, index) in datadiscipline"
-         :key="index"
-         >
+        <v-card
+          color="grey lighten-4"
+          class="mx-auto card"
+          max-width="900"
+          v-for="(discipline, index) in datadiscipline"
+          :key="index"
+        >
           <v-row>
             <v-col cols="12" sm="2" class="logo">
               <v-icon color="orange" size="60px"> mdi-alert </v-icon>
@@ -17,18 +17,18 @@
             <v-divider vertical class="ma-3" color="white"></v-divider>
             <v-col cols="12" sm="2">
               <v-avatar class="ma-3" size="120" tile>
-               <v-img :src=" url + discipline.students.picture "></v-img>
+                <v-img :src="url + discipline.students.picture"></v-img>
               </v-avatar>
             </v-col>
             <v-col cols="12" sm="3" class="text-center data">
-              <h3> {{discipline.students.first_name}}  {{discipline.students.last_name}} </h3>
+              <h3>
+                {{ discipline.students.first_name }}
+                {{ discipline.students.last_name }}
+              </h3>
               <span>WEB-A 2021</span><br />
-             
             </v-col>
             <v-col cols="12" sm="3" class="date">
-              <v-chip class="ma-2" color="yellow darken-4">2021-01-12</v-chip
-              >
-             
+              <v-chip class="ma-2" color="yellow darken-4">2021-01-12</v-chip>
             </v-col>
             <v-col cols="12" sm="2">
               <div class="i_con">
@@ -53,13 +53,13 @@
 <script>
 export default {
   props: ["datadiscipline"],
- 
+
   data() {
     return {
-       url: "http://127.0.0.1:8000/storage/images/",
+      url: "http://127.0.0.1:8000/storage/images/",
     };
-    
   },
+  
 };
 </script>
 <style scoped>
